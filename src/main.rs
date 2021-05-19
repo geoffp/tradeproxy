9af630f1652b11a4c8b9fdd7c1e2de83ceaa82ba
Geoff Pursell <geoffp@gmail.com>
2021-05-19 14:13:20 -0500

    A couple unit tests!

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use serde::Deserialize;
 use std::convert::Infallible;
 use std::result::Result;
-use warp::http::StatusCode;
-use warp::{Filter, Rejection, Reply};
+use warp::{http::StatusCode, Filter, Rejection, Reply};
 
 #[derive(Deserialize, Debug)]
 pub struct Request {
@@ -10,32 +9,73 @@ pub struct Request {
     pub contracts: String,
 }
 
+fn get_json() -> impl Filter<Extract = ((),), Error = warp::Rejection> + Copy {
+    warp::path!("trade")
+        .and(warp::post())
+        .and(warp::body::json())
+        .map(|req: Request| {
+            println!("Successful request: {:?}", req);
+        })
+}
+
+/*Ok(warp::reply::with_status(
+    "Success!",
+    StatusCode::OK,
+)) */
+
 #[tokio::main]
 async fn main() {
     println!("Tradeproxy starting up!");
 
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("trade")
-        .and(warp::post())
-        // if it's JSON...
-        .and(warp::body::json())
-        .map(|req: Request| {
-            println!("Got a request! {:?}", req);
-            warp::reply()
-        })
+    let api = get_json()
+        .map(|()| Ok(warp::reply::with_status("Success!", StatusCode::OK)))
         .recover(handle_error);
 
-    warp::serve(hello).run(([0, 0, 0, 0], 3137)).await;
+    // .map(|reply: warp::reply::WithStatus<, bytes: Bytes| -> _ {
+    //     let bad_bytes = format!("Bad request content: {:?}", bytes);
+    //     println!("{}", bad_bytes);
+    // });
+
+    // .or(warp::body::bytes())
+    // .map(|reply: Response<Reply>, bytes: Bytes| -> _ {
+    //     let bad_bytes = format!("Bad request content: {:?}", bytes);
+    //     println!("{}", bad_bytes);
+    // });
+
+    warp::serve(api).run(([0, 0, 0, 0], 3137)).await;
 }
 
 async fn handle_error(err: Rejection) -> Result<impl Reply, Infallible> {
-    let err_text = format!(
-        "Whoa! There was a problem with parsing the JSON, or something: {:?}",
-        err
+    let err_text = format!("Whoa, bad JSON: {:?}", err);
+
+    eprintln!("{}", err_text);
+
+    Ok(warp::reply::with_status(err_text, StatusCode::BAD_REQUEST))
+}
+
+
+#[tokio::test]
+async fn it_accepts_good_json() {
+    let filter = get_json();
+    assert!(
+        warp::test::request()
+            .path("/trade")
+            .method("POST")
+            .body("{\"action\": \"foo\", \"contracts\": \"bar\"}")
+            .matches(&filter)
+            .await
     );
-    println!("{}", err_text);
-    Ok(warp::reply::with_status(
-        err_text,
-        StatusCode::BAD_REQUEST,
-    ))
+}
+
+#[tokio::test]
+async fn it_rejects_bad_json() {
+    let filter = get_json();
+    assert!(
+        !warp::test::request()
+            .path("/trade")
+            .method("POST")
+            .body("blah blah blah")
+            .matches(&filter)
+            .await
+    );
 }
